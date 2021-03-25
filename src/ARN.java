c380888c8752f802985cf537c425c01fa9d49f92
justine66 <justine.nourry@u-psud.fr>
2021-03-25 15:40:38 +0100

    creation class test pour tester les methodes

--- src/ARN.java
+++ src/ARN.java
@@ -1,4 +1,3 @@
-import static com.sun.tools.doclint.Entity.and;
 
 public class ARN {
     private String sequence;
@@ -13,10 +12,30 @@ public class ARN {
         this.appariement = a;
         this.sequence = s;
     }
+
+    public String getAppariement() {
+        return appariement;
+    }
+
+    public String getSequence() {
+        return sequence;
+    }
+
+    /**
+     * verifie si this a le meme appariement que l'autre ARN
+     * @param a appariement de l'autre ARN
+     * @return vrai si meme appariement
+     */
     public boolean is_appiered(String a){
         return this.appariement.equals(a);
     }
 
+    /**
+     * verifie si this a le meme appariement et la meme sequence que l'autre ARN
+     * @param a appariement de l'autre ARN
+     * @param s sequence de l'autre ARN
+     * @return vrai si
+     */
     public boolean is_appiered(String a, String s){
         return this.appariement.equals(a) && this.sequence.equals(s);
     }
---
+++ src/test.java
@@ -0,0 +1,16 @@
+public class test {
+
+    public static void main(String[] args) {
+        ARN arn1 = new ARN("AAUU", "(())");
+        ARN arn2 = new ARN("AAAU", "(--)");
+
+        /* test de la methode is_appiered(a)*/
+        System.out.println(arn1.is_appiered(arn2.getAppariement()));
+        System.out.println(arn1.is_appiered(arn1.getAppariement()));
+
+        /* test de la methode is_appiered(a, s)*/
+        System.out.println(arn1.is_appiered(arn2.getAppariement(), arn2.getSequence()));
+        System.out.println(arn1.is_appiered(arn1.getAppariement(), arn1.getSequence()));
+
+    }
+}
