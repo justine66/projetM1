b05358a64c33e1aa5758ab1f2bf7a16262b71326
justine66 <justine.nourry@u-psud.fr>
2021-03-25 16:48:33 +0100

    debut de la classe Arbre

--- src/Arbre.java
+++ src/Arbre.java
@@ -1,3 +1,27 @@
-public class Arbre {
+import java.util.ArrayList;
 
+public class Arbre {
+    private ArrayList<Arbre> noeuds;
+
+    /**
+     * constructeur
+     */
+    public Arbre (){
+        this.noeuds = new ArrayList<Arbre>();
+    }
+
+    /**
+     * permet de recuperer les noeuds
+     */
+    public ArrayList<Arbre> getNoeuds() {
+        return noeuds;
+    }
+
+   /*  il va falloir creer notre propre split je pense
+   public void create_arbre (ARN arn){
+        String a = arn.getAppariement().split("(");
+        for (int i = 0; i< a.length(); i++){
+
+        }
+    }*/
 }
--- src/test.java
+++ src/test.java
@@ -3,6 +3,10 @@ public class test {
     public static void main(String[] args) {
         ARN arn1 = new ARN("AAUU", "(())");
         ARN arn2 = new ARN("AAAU", "(--)");
+        ARN arn3 = new ARN("AAAU", "-(-)");
+
+
+
 
         /* test de la methode is_appiered(a)*/
         System.out.println(arn1.is_appiered(arn2.getAppariement()));
