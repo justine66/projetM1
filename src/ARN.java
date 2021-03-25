6f375c073c9ef9bf8f1b70b38c6bc4973d4337c8
justine66 <justine.nourry@u-psud.fr>
2021-03-25 15:05:57 +0100

    debut des methodes

--- src/ARN.java
+++ src/ARN.java
@@ -1,2 +1,24 @@
+import static com.sun.tools.doclint.Entity.and;
+
 public class ARN {
+    private String sequence;
+    private String appariement;
+
+    /**
+     * constructeur
+     * @param a appariement des bases
+     * @param s sequence de l'ARN
+     **/
+    public ARN(String s, String a){
+        this.appariement = a;
+        this.sequence = s;
+    }
+    public boolean is_appiered(String a){
+        return this.appariement.equals(a);
+    }
+
+    public boolean is_appiered(String a, String s){
+        return this.appariement.equals(a) && this.sequence.equals(s);
+    }
+
 }
