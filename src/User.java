6c56233e0fc9707efb133acc264f15a298472df5
justine66 <justine.nourry@u-psud.fr>
2021-04-15 07:59:42 +0200

    ajout de bouttons delete mais pb de resize de la fenetre

--- src/User.java
+++ src/User.java
@@ -5,6 +5,7 @@ import java.io.File;
 import static java.lang.Math.max;
 
 public class User extends JDialog {
+
     private JPanel JPane1;
     private JPanel contentPane1;
     private JPanel contentPane2;
@@ -18,6 +19,8 @@ public class User extends JDialog {
     private JLabel ap1;
     private JLabel seq2;
     private JLabel ap2;
+    private JButton delete1;
+    private JButton delete2;
 
     public User() {
         setTitle("Projet M1 BIBS");
@@ -28,7 +31,34 @@ public class User extends JDialog {
             @Override
             public void actionPerformed(ActionEvent e) { ajouter(); }
         });
+        delete1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) { delete(1); }
+        });
 
+        delete2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) { delete(2); }
+        });
+
+
+    }
+
+    private void delete(int i) {
+        if (i == 1){
+            Worker w = new Worker("", this.ap1);
+            w.execute();
+            w = new Worker("",this.seq1);
+            w.execute();
+        } else if (i == 2){
+            Worker w = new Worker("", this.ap2);
+            w.execute();
+            w = new Worker("",this.seq2);
+            w.execute();
+        } else {
+            Worker w = new Worker("attention pb d'appel de la fonction delete", this.message);
+            w.execute();
+        }
 
     }
 
@@ -36,43 +66,63 @@ public class User extends JDialog {
         String seq = sequence.getText();
         String ap = appariement.getText();
         String fichier = file.getText();
+        Worker m = new Worker("", this.message);
+        m.execute();
         if (seq.length() == 0){
-            Worker m = new Worker("veuillez remplir le champ sequence", this.message);
-            m.execute();
+            Worker w = new Worker("veuillez remplir le champ sequence", this.message);
+            w.execute();
         }
         else if (ap.length() == 0 && fichier.length() != 0){
             File f = new File(fichier);
             ARN arn = new ARN(seq, f);
-            if (this.ap1.equals("")) {
-                Worker worker = new Worker(arn.getAppariement(), this.ap1);
+            if (this.ap1.getText().equals("") || this.ap2.getText().equals("")) {
+                if (this.ap1.getText().equals("")) {
+                    Worker worker = new Worker(arn.getAppariement(), this.ap1);
+                    worker.execute();
+                    Worker worker1 = new Worker(arn.getSequence(), this.seq1);
+                    worker1.execute();
+                } else {
+                    Worker worker = new Worker(arn.getAppariement(), this.ap2);
+                    worker.execute();
+                    Worker worker1 = new Worker(arn.getSequence(), this.seq2);
+                    worker1.execute();
+                }
+            } else {
+                Worker worker = new Worker("veuillez reinitialiser arn 1 ou 2", this.message);
                 worker.execute();
-                Worker worker1 = new Worker(arn.getSequence(), this.seq1);
-                worker1.execute();
-            }else{
-                Worker worker = new Worker(arn.getAppariement(), this.ap2);
-                worker.execute();
-                Worker worker1 = new Worker(arn.getSequence(), this.seq2);
-                worker1.execute();
             }
         }else if (ap.length() != 0 && fichier.length() == 0){
             ARN arn = new ARN(seq, ap);
-            if (this.ap1.getText().equals("")) {
-                Worker worker = new Worker(arn.getAppariement(), this.ap1);
+            if (this.ap1.getText().equals("") || this.ap2.getText().equals("")) {
+                if (this.ap1.getText().equals("")) {
+                    Worker worker = new Worker(arn.getAppariement(), this.ap1);
+                    worker.execute();
+                    Worker worker1 = new Worker(arn.getSequence(), this.seq1);
+                    worker1.execute();
+                } else {
+                    Worker worker = new Worker(arn.getAppariement(), this.ap2);
+                    worker.execute();
+                    Worker worker1 = new Worker(arn.getSequence(), this.seq2);
+                    worker1.execute();
+                }
+                if (max(ap1.getText().length(),ap2.getText().length())> 10 )
+                {
+                    setSize(max(getWidth() + ap1.getWidth(), getWidth() + ap2.getWidth()), getHeight());
+                }
+            } else {
+                Worker worker = new Worker("veuillez reinitialiser arn 1 ou 2", this.message);
                 worker.execute();
-                Worker worker1 = new Worker(arn.getSequence(), this.seq1);
-                worker1.execute();
-                setSize(max(getWidth() + ap1.getWidth(),getWidth() + ap2.getWidth()),max(getHeight() + ap1.getHeight(),getHeight() + ap2.getHeight()));
-            }else{
-                Worker worker = new Worker(arn.getAppariement(), this.ap2);
-                worker.execute();
-                Worker worker1 = new Worker(arn.getSequence(), this.seq2);
-                worker1.execute();
-                setSize(max(getWidth() + ap1.getWidth(),getWidth() + ap2.getWidth()),max(getHeight() + ap1.getHeight(),getHeight() + ap2.getHeight()));
             }
+
+
         }else{
-            Worker m = new Worker("veuillez remplir le champ appariement ou fichier", this.message);
-            m.execute();
+            Worker w = new Worker("veuillez remplir le champ appariement ou fichier", this.message);
+            w.execute();
         }
+        System.out.println("$$$");/*
+        System.out.println(contentPane3.getWidth());
+        System.out.println(max(ap1.getWidth(),ap2.getWidth())> getWidth()- (delete1.getWidth()));
+        System.out.println(ap2.getSize());*/
     }
 
 
@@ -80,6 +130,7 @@ public class User extends JDialog {
         User dialog = new User();
         dialog.pack();
         dialog.setVisible(true);
+        System.out.println(dialog.getWidth());
         System.exit(0);
     }
 
